772d6bdd14ecd472c9a336ba4f60fdf29cbbb774
kajugadaniels <kajugadaniels@gmail.com>
2024-07-02 17:42:00 +0200

    Admin login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function getUsers()
+    {
+        try {
+            $users = User::all();
+
+            return response()->json(['users' => $users], 200);
+        } catch (\Exception $e) {
+            // Handle any unexpected errors
+            return response()->json(['message' => 'Internal Server Error'], 500);
+        }
+    }
+
+    public function login(Request $request)
+    {
+        try {
+            $request->validate([
+                'email' => 'required|email|exists:users,email',
+                'password' => 'required'
+            ], [
+                'email.exists' => 'The selected email is invalid or the account does not exist.',
+            ]);
+
+            $user = User::where('email', $request->email)->first();
+
+            if (!$user || !Hash::check($request->password, $user->password)) {
+                throw ValidationException::withMessages([
+                    'credentials' => ['Incorrect credentials. Please try again.'],
+                ]);
+            }
+
+            $token = $user->createToken($user->name)->plainTextToken;
+
+            return response()->json([
+                'message' => 'Login successful',
+                'token' => $token,
+                'name' => $user,
+            ], 200);
+        } catch (ValidationException $e) {
+            return response()->json(['errors' => $e->errors()], 422);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/test', function () {
+    return 'API is working';
+});
+
+Route::get('/users', [AuthController::class, 'getUsers']);
+
+Route::post('/login', [AuthController::class, 'login']);
