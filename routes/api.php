2af889913fdcbdb437a334d370de675a2b2158b7
kajugadaniels <kajugadaniels@gmail.com>
2024-07-05 17:21:24 +0200

    New changes

--- routes/api.php
+++ routes/api.php
@@ -13,11 +13,13 @@ Route::get('/users', [AuthController::class, 'getUsers'])->middleware('auth:sanc
 
 Route::get('/tour-packages', [TourPackageController::class, 'index']);
 Route::get('/tour-package/{slug}', [TourPackageController::class, 'show']);
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/tour-package', [TourPackageController::class, 'store']);
-    Route::put('/tour-package/{slug}', [TourPackageController::class, 'update']);
-    Route::delete('/tour-package/{slug}', [TourPackageController::class, 'destroy']);
-});
+
+Route::post('/tour-package', [TourPackageController::class, 'store']);
+Route::put('/tour-package/{slug}', [TourPackageController::class, 'update']);
+Route::delete('/tour-package/{slug}', [TourPackageController::class, 'destroy']);
+
+// Route::middleware('auth:sanctum')->group(function () {
+// });
 
 Route::post('/tour-package/{tourPackageId}/booking', [BookingController::class, 'store']);
 Route::get('/bookings', [BookingController::class, 'getBookings'])->middleware('auth:sanctum');
